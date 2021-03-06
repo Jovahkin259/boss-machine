2826fcb0d64884ae09e751f7d1186fdf89459335
Joshua Astle <jovahkin259@gmail.com>
2021-03-06 20:03:41 +0000

    Add correct db path

--- server/routes/minions.js
+++ server/routes/minions.js
@@ -1,25 +1,23 @@
-import { getAllFromDatabase } from 'db.js'
+const db = require('../db')
 const express = require('express')
 const minionsRouter = express.Router()
 
 // Get all minions
 minionsRouter.get('/', (req, res, next) => {
-  res.send(getAllFromDatabase('minions'))
+  res.send(db.getAllFromDatabase('minions'))
 })
 
 const validateMinion = (req, res, next) => {
   const newMinion = req.body
-
   if (typeof newMinion.name !== 'string' || typeof newMinion.title !== 'string' ||
         typeof newMinion.salary !== 'number') {
     res.status(400).send()
   } else {
     req.newMinion = newMinion
+    next()
   }
 }
 
 // Create a new minion
-minionsRouter.post('/', validateMinion, (req, res, next) => {
-
-})
+minionsRouter.post('/', validateMinion, (req, res, next) => {})
 module.exports = minionsRouter
