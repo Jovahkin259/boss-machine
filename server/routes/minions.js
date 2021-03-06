2515faa44cd627172a51f257c102e60bc0288d07
Joshua Astle <jovahkin259@gmail.com>
2021-03-06 19:43:56 +0000

    Added minion validation

--- server/routes/minions.js
+++ server/routes/minions.js
@@ -7,6 +7,19 @@ minionsRouter.get('/', (req, res, next) => {
   res.send(getAllFromDatabase('minions'))
 })
 
-// Create a new minion
+const validateMinion = (req, res, next) => {
+  const newMinion = req.body
 
+  if (typeof newMinion.name !== 'string' || typeof newMinion.title !== 'string' ||
+        typeof newMinion.salary !== 'number') {
+    res.status(400).send()
+  } else {
+    req.newMinion = newMinion
+  }
+}
+
+// Create a new minion
+minionsRouter.post('/', validateMinion, (req, res, next) => {
+
+})
 module.exports = minionsRouter
