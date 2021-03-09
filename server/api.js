73839ebc6fd824382a46f62291bd2e6015433c35
Joshua Astle <jovahkin259@gmail.com>
2021-03-09 21:55:22 +0000

    Added meetings route

--- server/api.js
+++ server/api.js
@@ -1,10 +1,14 @@
 const minionsRouter = require('./routes/minions')
+const ideasRouter = require('./routes/ideas')
+const meetingsRouter = require('./routes/meetings')
 const express = require('express')
 const apiRouter = express.Router()
 
 // minions router
 apiRouter.use('/minions', minionsRouter)
 // ideas router
-
+apiRouter.use('/ideas', ideasRouter)
 // meetings router
+apiRouter.use('/meetings', meetingsRouter)
+
 module.exports = apiRouter
