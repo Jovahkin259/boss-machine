3dfb97711f6ebe63ed0acc567942795510e842a4
Joshua Astle <jovahkin259@gmail.com>
2021-03-09 21:56:30 +0000

    add ideass route

---
+++ server/routes/ideas.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const ideasRouter = express.Router()
+const db = require('../db')
+
+// Check matching id parameters
+ideasRouter.param('ideaId', (req, res, next, ideaId) => {
+  const idea = db.getFromDatabaseById('ideas', ideaId)
+
+  if (!idea) {
+    res.status(404).send()
+  } else {
+    req.idea = idea
+    next()
+  }
+})
+
+// Get all ideas
+ideasRouter.get('/', (req, res, next) => {
+  try {
+    res.send(db.getAllFromDatabase('ideas'))
+  } catch (err) {
+    res.status(404).send(err)
+  }
+})
+
+// Get idea by id
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  res.send(req.idea)
+})
+
+// Update an idea by id
+ideasRouter.put('/:ideaId', (req, res, next) => {
+  const updatedIdea = db.updateInstanceInDatabase('ideas', req.body)
+
+  if (updatedIdea) {
+    res.send(updatedIdea)
+  } else {
+    res.status(404).send()
+  }
+})
+
+// Create a new idea
+ideasRouter.post('/', (req, res, next) => {
+  const newIdea = db.addToDatabase('ideas', req.body)
+  if (!newIdea) {
+    res.status(400).send()
+  } else {
+    res.status(201).send(newIdea)
+  }
+})
+
+// Delete an idea
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  const success = db.deleteFromDatabasebyId('ideas', req.params.ideaId)
+  if (!success) {
+    res.status(400).send()
+  } else {
+    res.status(204).send()
+  }
+})
+
+module.exports = ideasRouter
