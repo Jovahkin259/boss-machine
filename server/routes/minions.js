edc638af24b99a605146b98f353000d4d5e57d50
Joshua Astle <jovahkin259@gmail.com>
2021-03-09 21:56:46 +0000

    minion fix

--- server/routes/minions.js
+++ server/routes/minions.js
@@ -3,15 +3,25 @@ const express = require('express')
 const minionsRouter = express.Router()
 
 // Match parameters to
-minionsRouter.param('minionId', (req, res, next, id) => {
-  const minionId = db.getFromDatabaseById('minions', req.params.minionId)
-  if (!minionId) {
+minionsRouter.param('minionId', (req, res, next, minionId) => {
+  const minion = db.getFromDatabaseById('minions', minionId)
+  if (!minion) {
     res.status(404).send()
   } else {
-    req.minionId = minionId
+    req.minion = minion
     next()
   }
 })
+
+// Validate minions
+const validateMinion = (req, res, next) => {
+  const validMinion = req.body
+  if (typeof validMinion.name === 'string' && typeof validMinion.title === 'string' && typeof validMinion.salary === 'number') {
+    next()
+  } else {
+    res.status(400).send()
+  }
+}
 
 // Get all minions
 minionsRouter.get('/', (req, res, next) => {
@@ -24,12 +34,36 @@ minionsRouter.get('/', (req, res, next) => {
 
 // Get a minion by id
 minionsRouter.get('/:minionId', (req, res, next) => {
-  res.send(req.minionId)
+  res.send(req.minion)
 })
 
 // Update a minion
+minionsRouter.put('/:minionId', (req, res, next) => {
+  try {
+    const updatedMinion = db.updateInstanceInDatabase('minions', req.body)
+    res.send(updatedMinion)
+  } catch (err) {
+    res.status(400).send(err.message)
+  }
+})
 
 // Create a new minion
+minionsRouter.post('/', validateMinion, (req, res, next) => {
+  const newMinion = db.addToDatabase('minions', req.body)
 
+  if (newMinion) {
+    res.status(201).send(newMinion)
+  } else {
+    res.status(400).send()
+  }
+})
 //  Delete a minion
+minionsRouter.delete('/:minionId', (req, res, next) => {
+  const success = db.deleteFromDatabasebyId('minions', req.params.minionId)
+  if (!success) {
+    res.status(400).send()
+  } else {
+    res.status(204).send()
+  }
+})
 module.exports = minionsRouter
